786a26f3682d371e031a790cf3b44051244a9333
Alexander Ezhov <aezhov@inno.tech>
2022-06-23 16:23:16 +0300

    finalized comparing prod/dev

--- builder.js
+++ builder.js
@@ -1,27 +1,40 @@
-const { sassPlugin, postcssModules } = require("esbuild-sass-plugin");
+const fs = require('fs')
+const {sassPlugin, postcssModules} = require("esbuild-sass-plugin");
+const esbuild = require("esbuild")
 const isProd = process.env.NODE_ENV === "production"
+const dist = isProd ? 'dist' : "public"
 
 const builderParams = {
-  entryPoints: ["src/index.js"],
-  bundle: true,
-  loader: { ".js": "jsx", ".css": "css" },
-  outfile: "dist/index.js",
-  plugins: [
-    sassPlugin({
-      transform: postcssModules({}),
-    }),
-  ],
+    entryPoints: ["src/index.js"],
+    bundle: true,
+    loader: {".js": "jsx", ".css": "css"},
+    outfile: `${dist}/index.js`,
+    plugins: [
+        sassPlugin({
+            transform: postcssModules({}),
+        }),
+    ],
 };
 
+const runProd = async () => {
+    await fs.rmSync(dist, {recursive: true, force: true});
+    await fs.mkdirSync(dist)
+    await fs.copyFileSync('public/index.html', "dist/index.html")
+    await esbuild.build(builderParams)
+}
 
-isProd ? require("esbuild").build(builderParams) : require("esbuild")
-  .serve(
-    {
-      servedir: "dist",
-      port: 3000,
-    },
-    builderParams
-  )
-  .then((server) => {
-      console.log('\x1b[32m', `🟢 Serving at ${server.host}:${server.port}`)
-  })
+const runDev = async () => {
+    esbuild
+        .serve(
+            {
+                servedir: dist,
+                port: 3000,
+            },
+            builderParams
+        )
+        .then((server) => {
+            console.log('\x1b[32m', `🟢 Serving at ${server.host}:${server.port}`)
+        })
+}
+
+isProd ? runProd() : runDev()
